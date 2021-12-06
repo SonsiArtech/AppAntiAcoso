685e245cccbdc38d2a72f89d23a05958ee55b436
SonsoArt <nicolas.lara10@inacapmail.cl>
2021-12-06 00:23:55 -0300

    Se agrega menu e imagenes

--- app/src/main/java/com/example/anti_acoso/Home.java
+++ app/src/main/java/com/example/anti_acoso/Home.java
@@ -1,8 +1,13 @@
 package com.example.anti_acoso;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +15,45 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    //nos ayuda a colocar el menu en nuestras paginas
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menuop, menu);
+        return true;
+    }
+
+    //Codigo para seleccionar las opciones del menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.opHome:
+                Toast.makeText(this,"Home seleccionado",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opPublicacion:
+                Toast.makeText(this,"Publicaciones seleccionado",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opDenuncia:
+                Toast.makeText(this,"Denuncia seleccionado",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opNoticias:
+                Toast.makeText(this,"Noticias seleccionado",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opAyuda:
+                Toast.makeText(this,"Ayuda seleccionado",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opCerrarSesion:
+                Toast.makeText(this,"Cerrando Sesión",Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
